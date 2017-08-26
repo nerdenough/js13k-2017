dad39e9624220f73c736610142ddb8efea064030
Brendan Goodenough <brendan@goodenough.nz>
2017-08-26 13:54:18 +1200

    Add player

--- src/game.js
+++ src/game.js
@@ -7,6 +7,21 @@ $.previousTimestamp = 0
 $.tileSize = 64
 $.tiles = []
 
+// Player
+$.Player = function (x, y) {
+  this.x = x
+  this.y = y
+}
+
+$.Player.prototype.update = function (dt) {
+
+}
+
+$.Player.prototype.render = function () {
+  $.ctx.fillStyle = 'blue'
+  $.ctx.fillRect(this.x, this.y, 32, 48)
+}
+
 // Tiles
 $.Tile = function (x, y) {
   this.x = x
@@ -30,6 +45,9 @@ $.init = function () {
   $.canvas.width = $.width
   $.canvas.height = $.height
 
+  // Initialise player
+  $.player = new $.Player($.width / 2 - 16, $.height / 2 - 24)
+
   // Initialise tiles
   for (let i = 0; i < 100; i++) {
     for (let j = 0; j < 100; j++) {
@@ -40,11 +58,13 @@ $.init = function () {
 
 $.update = function (dt) {
   $.tiles.forEach(tile => tile.update(dt))
+  $.player.update(dt)
 }
 
 $.render = function () {
   $.ctx.clearRect(0, 0, $.width, $.height)
   $.tiles.forEach(tile => tile.render())
+  $.player.render()
 }
 
 $.loop = function (timestamp) {
